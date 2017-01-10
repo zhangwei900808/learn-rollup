1fe511c8b75dbdcaae44447de9ccc88f162006a0
zhangwei900808 <zw900808@gmail.com>
2017-01-10 15:10:22 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 import babel from "rollup-plugin-babel";
 import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -7,6 +11,12 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins:[
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
     babel({
       exclude:'node_modules/**'
     }),
@@ -14,6 +24,10 @@ export default {
       exclude:[
         'src/styles/**'
       ]
-    })
+    }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
   ]
 };
